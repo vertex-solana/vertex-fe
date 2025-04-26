a7876200ebaa8484d27ad9036ecc804b7a7d554c
vudx <dxv91999@gmail.com>
2025-04-26 22:24:34 +0700

    update-ui-display-indexer-and-user-info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,10 @@ const Home = () => {
   const [isOpenCreateModal, setIsOpenCreateModal] = useState(false);
   const [updatedIndexers, setUpdatedIndexers] = useState<IndexerResponse[]>([]);
 
+  const [selectedTypeIndexer, setSelectedTypeIndexer] = useState(
+    IndexerTypeEnum.All
+  );
+
   const handleSearch = (valueSearch: string) => {
     const filterIdl = indexers.filter((item) =>
       item.name.toLocaleLowerCase().includes(valueSearch)
@@ -94,58 +98,74 @@ const Home = () => {
           </Button>
         )}
 
-        <div className="rounded-2xl border border-border p-4 bg-[#0a0a0b]/90 w-full sm:max-w-[80%] min-h-[450px] flex items-center flex-col gap-y-1">
-          <div className="w-full">
+        <div className="flex flex-col w-full sm:max-w-[80%] relative">
+          <div className="z-[100] w-fit flex items-center px-3 pt-2 rounded-t-xl border border-border bg-[#0a0a0b]/90 border-b-[#0a0a0b]/90 -mb-[1px]">
+            {Object.values(IndexerTypeEnum).map((item) => (
+              <button
+                className={twJoin(
+                  "p-2 min-w-[74px] text-sm rounded-lg",
+                  item === selectedTypeIndexer && "bg-[#6d2ef4]"
+                )}
+                onClick={() => setSelectedTypeIndexer(item)}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+
+          <div className="w-fit absolute -top-1 right-0">
             <CommonInput
               startAdornment={<SearchIcon className="w-5 h-5" />}
               inputWrapperClassName={twJoin(
                 "!bg-[#1e2024]",
                 "overflow-hidden",
-                "rounded-full border-none",
-                "gap-x-2 h-[34px] !w-[220px] ml-auto"
+                "rounded-xl border-none",
+                "gap-x-2 h-[40px] !w-[220px] ml-auto"
               )}
               onChange={(e) => handleSearch(e.target.value.toLowerCase())}
               className="text-sm"
               placeholder="Search by name..."
             />
           </div>
-          {updatedIndexers.length > 0 ? (
-            <div className="w-full ">
-              <div className="flex items-center w-full grid grid-cols-[20%_70%_10%] px-4 py-2 bg-characterBackground2 rounded-t-lg text-sm text-neutral5">
-                <p>Name</p>
-                <p>Description</p>
-                <p>Cluster</p>
-              </div>
-              <div className="flex flex-col sm:h-[330px] overflow-y-auto">
-                {updatedIndexers?.map((indexer) => (
-                  <div
-                    key={indexer.id}
-                    className="flex items-center w-full grid grid-cols-[20%_70%_10%] px-4 py-4 border-b border-neutral6 font-medium text-sm sm:text-base hover:bg-white/5"
-                  >
-                    <p>{indexer.name}</p>
-                    <button
-                      onClick={() => {
-                        router.push(`/${indexer.id}`);
-                      }}
+          <div className="w-full rounded-b-xl border border-border p-4 bg-[#0a0a0b]/90  min-h-[450px] flex items-center flex-col gap-y-1">
+            {updatedIndexers.length > 0 ? (
+              <div className="w-full ">
+                <div className="flex items-center w-full grid grid-cols-[20%_70%_10%] px-4 py-2 bg-characterBackground2 rounded-t-lg text-sm text-neutral5">
+                  <p>Name</p>
+                  <p>Description</p>
+                  <p>Cluster</p>
+                </div>
+                <div className="flex flex-col sm:h-[330px] overflow-y-auto">
+                  {updatedIndexers?.map((indexer) => (
+                    <div
+                      key={indexer.id}
+                      className="flex items-center w-full grid grid-cols-[20%_70%_10%] px-4 py-4 border-b border-neutral6 font-medium text-sm sm:text-base hover:bg-white/5"
                     >
+                      <p>{indexer.name}</p>
+                      <button
+                        onClick={() => {
+                          router.push(`/${indexer.id}`);
+                        }}
+                      >
+                        <p className="text-primary5 text-start truncate">
+                          {indexer.description}
+                        </p>
+                      </button>
                       <p className="text-primary5 text-start truncate">
-                        {indexer.description}
+                        {indexer.cluster}
                       </p>
-                    </button>
-                    <p className="text-primary5 text-start truncate">
-                      {indexer.cluster}
-                    </p>
-                  </div>
-                ))}
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className="w-full h-full ">
-              <p className="mx-auto my-auto w-full h-full text-center">
-                No indexers found. Please create one.
-              </p>
-            </div>
-          )}
+            ) : (
+              <div className="w-full h-full ">
+                <p className="mx-auto my-auto w-full h-full text-center">
+                  No indexers found. Please create one.
+                </p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
@@ -162,3 +182,8 @@ const Home = () => {
 };
 
 export default Home;
+
+export enum IndexerTypeEnum {
+  All = "All",
+  Owner = "Owner",
+}
